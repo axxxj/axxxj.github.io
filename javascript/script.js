8f4aa77a7cc68ee1198e485cad9dc281dfe94e4d
ANDREW JAMES <axxxj10@gmail.com>
2014-09-05 23:20:56 +0100

    no message

--- javascript/script.js
+++ javascript/script.js
@@ -9,7 +9,12 @@ $(document).ready(function() {
 			  preload: 	[0,1]
 			}
 		});
-	}); 
+	});
+
+	setJigsawImageOne();
+	setToolsImageOne();
+
+
 
 	// if ($(window).width() > 1024) {
 
@@ -19,3 +24,27 @@ $(document).ready(function() {
 
 
 });
+
+function setJigsawImageOne() {
+    $('#imageSwap').fadeIn(200).html('<img src="images/tech/Jigsaw_01.jpg" />').delay(3000).fadeOut(200, function () { setJigsawImageTwo(); });
+}
+
+function setJigsawImageTwo() {
+    $('#imageSwap').fadeIn(200).html('<img src="images/tech/Jigsaw_02.jpg" />').delay(3000).fadeOut(200, function () { setJigsawImageThree(); });
+}
+
+function setJigsawImageThree() {
+    $('#imageSwap').fadeIn(200).html('<img src="images/tech/Jigsaw_03.jpg" />').delay(3000).fadeOut(200, function () { setJigsawImageOne(); });
+}
+
+function setToolsImageOne() {
+    $('#imageSwap2').fadeIn(200).html('<img src="images/tech/Tools_01.jpg" />').delay(2500).fadeOut(200, function () { setToolsImageTwo(); });
+}
+
+function setToolsImageTwo() {
+    $('#imageSwap2').fadeIn(200).html('<img src="images/tech/Tools_02.jpg" />').delay(2500).fadeOut(200, function () { setToolsImageThree(); });
+}
+
+function setToolsImageThree() {
+    $('#imageSwap2').fadeIn(200).html('<img src="images/tech/Tools_03.jpg" />').delay(2500).fadeOut(200, function () { setToolsImageOne(); });
+}
